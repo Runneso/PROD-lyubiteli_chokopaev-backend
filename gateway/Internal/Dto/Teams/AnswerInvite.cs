73f0b80488486637d852d04d22865cc98e5c7fda
Nikita <ns_stepanov@inbox.ru>
2024-04-03 06:39:16 +0300

    Update gateway

---
+++ gateway/Internal/Dto/Teams/AnswerInvite.cs
@@ -0,0 +1,8 @@
+namespace Gateway.Internal.Dto 
+{
+    public class AnswerInviteDto
+    {
+        public int invite_id { get; set; }
+        public bool isAccepted { get; set; }
+    }
+}
--- gateway/Internal/Interfaces/ITeamsService.cs
+++ gateway/Internal/Interfaces/ITeamsService.cs
@@ -13,5 +13,9 @@ namespace Gateway.Internal.Interfaces
         Task DeleteTag(int team_id, string tag, string token);
         Task CreateInvite(CreateInviteDto dto, string token);
         Task<List<InviteDto>> GetInvites(GetInviteDto dto, string token);
+        Task AnswerInvite(AnswerInviteDto dto, string token);
+        Task<List<TeamDto>> GetPossible(int eventId, int offset,string token);
+        Task<TeamDto> My(int event_id, string token);
+        Task DeleteUser(int t, int u, string token);
     }
 }
